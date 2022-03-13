d048db6d8c345c458fe6556921cdd578222bfef6
lsabor <lukesabor@gmail.com>
2022-03-13 12:32:38 -0400

    added todo template

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -10,3 +11,10 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_todo_home_page_view(self):
         found = resolve('/todo/')
         self.assertEqual(found.func, index)
+
+    def test_todo_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- todo/views.py
+++ todo/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    return render(request, 'polls/home_page.html')
