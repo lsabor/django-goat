b7cf24137f997dbfd08c0d01d8a8183ad5caf4f5
lsabor <lukesabor@gmail.com>
2022-03-13 12:15:19 -0400

    updated todo tests to check if index is home page

--- todo/tests.py
+++ todo/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from todo.views import index
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    def test_root_url_resolves_to_todo_home_page_view(self):
+        found = resolve('/todo/')
+        self.assertEqual(found.func, index)
