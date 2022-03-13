74d3bb214ceffb1ef5d932e060bda1549a54b547
lsabor <lukesabor@gmail.com>
2022-03-13 18:24:00 -0400

    added test cases to polls

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,26 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import resolve
+from django.template.loader import render_to_string
+
+from polls.views import index
 
 # Create your tests here.
+
+class PoleDanceTest(TestCase):
+
+    def test_root_url_resolves_to_polls_home_page_view(self):
+        found = resolve('/polls/')
+        self.assertEqual(found.func, index)
+
+    def test_poll_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        expected_html = render_to_string('polls/index.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+
+
+
+
