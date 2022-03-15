8ac62ffaa4f3ea2ae4d001ceef12e861a93a0545
lsabor <lukesabor@gmail.com>
2022-03-15 09:35:09 -0400

    now messing with fetch

--- playground/tests.py
+++ playground/tests.py
@@ -5,3 +5,10 @@ from django.test import TestCase
 
 # I just added this to storefront2, but not storefront1, let's see what happens!
 
+
+
+
+# adding this weird text to sf1, not sf2
+# fetch related
+
+
