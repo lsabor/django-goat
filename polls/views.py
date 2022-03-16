a5fac0ebfe8494fcb7a6b2137b9d0642f2d1dd18
lsabor <lukesabor@gmail.com>
2022-03-15 20:58:16 -0400

    updated vote view x2

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
